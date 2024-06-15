9f7b985b375eb5ab58ad11216bdfa4047cff7acc
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-06-14 20:44:36 -0400

    date range

--- app/controllers/solid_apm/transactions_controller.rb
+++ app/controllers/solid_apm/transactions_controller.rb
@@ -5,10 +5,10 @@ module SolidApm
     TransactionAggregation = Struct.new(:name, :tmp, :latency, :percentile_95, :impact)
 
     def index
-      @aggregated_transactions = Transaction.where(created_at: 1.hour.ago..).group_by(&:name)
+      @aggregated_transactions = Transaction.where(created_at: from_to_range).group_by(&:name)
       @aggregated_transactions.transform_values! do |transactions|
         latency = transactions.map(&:duration).sum / transactions.size
-        tmp = transactions.size.to_f / 60
+        tmp = transactions.size.to_f / ((from_to_range.end - from_to_range.begin) / 60).to_i
         impact = latency * tmp
         percentile_95 = transactions[transactions.size * 0.95].duration
         TransactionAggregation.new(
@@ -26,7 +26,8 @@ module SolidApm
       # Normalize impact 0-100
       @aggregated_transactions.each do |_, aggregation|
         normalized_impact = ((aggregation.impact - min_impact) / (max_impact - min_impact)) * 100
-        aggregation.impact = normalized_impact.to_i
+        normalized_impact = 0 if normalized_impact.nan?
+        aggregation.impact = normalized_impact.to_i || 0
       end
       @aggregated_transactions = @aggregated_transactions.sort_by { |_, v| -v.impact }.to_h
     end
@@ -47,7 +48,7 @@ module SolidApm
 
     def count_by_minutes
       scope = Transaction.all.order(timestamp: :desc)
-                 .where(created_at: 1.hour.ago..)
+                 .where(created_at: from_to_range)
 
       if params[:name].present?
         scope = scope.where(name: params[:name])
@@ -55,6 +56,16 @@ module SolidApm
 
       render json: scope.group_by { |t| t.created_at.beginning_of_minute }
            .transform_values!(&:count)
+    end
+
+    private
+
+    def from_to_range
+      params[:from_value] ||= 60
+      params[:from_unit] ||= 'minutes'
+      from = params[:from_value].to_i.public_send(params[:from_unit].to_sym).ago
+      to = Time.current
+      (from..to)
     end
   end
 end
