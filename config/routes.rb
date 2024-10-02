88cf642a7fa073293df236a84d128e82bfd0c2dc
Jean-Francis Bastien <bhacaz@gmail.com>
2024-10-02 09:29:43 -0400

    Better time aggregate for bar graph (#3)

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ SolidApm::Engine.routes.draw do
   root 'transactions#index'
 
   get 'transactions', to: 'transactions#index'
-  get 'transactions/count_by_minutes',
-      to: 'transactions#count_by_minutes',
-      as: 'transactions_count_by_minutes',
+  get 'transactions/count_time_aggregations',
       default: { format: 'json' }
 
   get 'transactions/:id', to: 'transactions#show', as: 'transaction', constraints: { id: /\d+/ }
