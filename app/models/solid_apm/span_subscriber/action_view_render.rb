df63ed097a524717d07925c752169ab15df55f64
Jean-Francis Bastien <jfbastien@petalmd.com>
2025-06-23 14:49:51 -0400

    clean action_view subscriber

--- app/models/solid_apm/span_subscriber/action_view_render.rb
+++ app/models/solid_apm/span_subscriber/action_view_render.rb
@@ -9,29 +9,6 @@ module SolidApm
         identifier = payload[:identifier]
         clean_trace(identifier)
       end
-
-      private
-
-      def sanitize_path(path)
-        if path.start_with? Rails.root.to_s
-          app_path(path)
-        else
-          gem_path(path)
-        end
-      end
-
-      def app_path(path)
-        return unless path.start_with? Rails.root.to_s
-
-         path[Rails.root.to_s.length + 1, path.length]
-      end
-
-      def gem_path(path)
-        root = Gem.path.find { |gp| path.start_with? gp }
-        return unless root
-
-        path[root.length + 1, path.length]
-      end
     end
   end
 end
